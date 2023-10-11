058fa8b7cc9eb23153a02b29191ded390ef3fab7
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2023-10-10 19:17:50 -0500

    fix

--- app/settings/account-form-new.tsx
+++ app/settings/account-form-new.tsx
@@ -1,10 +1,10 @@
-"use client"
+// "use client"
  
-import { zodResolver } from "@hookform/resolvers/zod"
-import * as z from "zod"
+// import { zodResolver } from "@hookform/resolvers/zod"
+// // import * as z from "zod"
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
-})
+// const formSchema = z.object({
+//     username: z.string().min(2, {
+//       message: "Username must be at least 2 characters.",
+//     }),
+// })
